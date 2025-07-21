3cf73f11d32d0b9c848bc612bea55cf401891931
ElieHanna95 <elitohanna@gmail.com>
2025-07-21 16:47:00 +0200

    Update Elie saves.py

--- Elie saves.py
+++ Elie saves.py
@@ -35,6 +35,46 @@ class FileReader:
         with open(new_file, 'w') as fout, open(self._filename, 'r') as f1, open(other._filename, 'r') as f2:
             fout.writelines(f1.readlines() + f2.readlines())
         return FileReader(new_file)
+    def __add__(self, other):
+        base1 = os.path.basename(self._filename)
+        base2 = os.path.basename(other._filename)
+        dir1 = os.path.dirname(self._filename)
+        new_file = os.path.join(dir1, f"{base1}_plus_{base2}.txt")
+        with open(new_file, 'w') as fout, open(self._filename, 'r') as f1, open(other._filename, 'r') as f2:
+            fout.writelines(f1.readlines() + f2.readlines())
+        return FileReader(new_file)
+    def concat_files(self, *filenames):
+        with open(self._filename, 'a') as fout:
+            for fname in filenames:
+                with open(fname, 'r') as fin:
+                    fout.write(fin.read())
+    def line_generator(self):
+        with open(self._filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                yield line.rstrip('\n')
+    def deco(color):
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                print(ANSI_COLORS.get(color, ""), end="")
+                result = func(*args, **kwargs)
+                print(ANSI_COLORS["reset"], end="")
+                return result
+            return wrapper
+        return decorator
+    class UpperCaseFileReader(FileReader):
+        def line_generator(self):
+            with open(self._filename, 'r', encoding='utf-8') as file:
+                for line in file:
+                    yield line.rstrip('\n').upper()
+    class UpperCaseFileReader(FileReader):
+        def line_generator(self):
+            with open(self._filename, 'r', encoding='utf-8') as file:
+                for line in file:
+                    yield line.rstrip('\n').upper()
+        def __str__(self):
+            with open(self._filename, 'r', encoding='utf-8') as f:
+                return f.read().upper()
+
 
 
 
