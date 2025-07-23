172ab6620a16ae812b9ee4eb5d1e03e464eed5af
ElieHanna95 <elitohanna@gmail.com>
2025-07-23 21:47:14 +0200

    making the code work

--- main1.py
+++ main1.py
@@ -4,6 +4,28 @@ import time
 import pytest
 from functools import wraps
 
+
+ANSI_COLORS = {
+    "red": "\033[91m",
+    "green": "\033[92m",
+    "yellow": "\033[93m",
+    "cyan": "\033[96m",
+    "magenta": "\033[95m",
+    "reset": "\033[0m"
+}
+
+
+def deco(color):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(ANSI_COLORS.get(color, ""), end="")
+            result = func(*args, **kwargs)
+            print(ANSI_COLORS["reset"], end="")
+            return result
+        return wrapper
+    return decorator
+
+
 class FileReader:
     def __init__(self, filename):
         self._filename = filename
@@ -16,63 +38,45 @@ class FileReader:
     def filename(self, value):
         self._filename = value
     def line_generator(self):
-        with open(self._filename, 'r') as file:
+        with open(self._filename, 'r', encoding = 'utf-8') as file:
             for line in file:
                 yield line.rstrip('\n')
     @staticmethod
     def count_lines(filename):
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding= 'utf-8') as f:
             return sum(1 for _ in f)
     @classmethod
     def from_string(cls, content, outfilename):
-        with open(outfilename, 'w') as f:
+        with open(outfilename, 'w', encoding= 'utf-8') as f:
             f.write(content)
         return cls(outfilename)
     def __str__(self):
         lines = []
-        with open(self._filename, 'r') as f:
+        with open(self._filename, 'r', encoding='utf-8') as f:
             for i, line in enumerate(f):
                 if i >= 3:
                     break
                 lines.append(line.rstrip('\n'))
         return "\n".join(lines)
     def __add__(self, other):
-        new_file = f"{self._filename}_plus_{other._filename}.txt"
-        with open(new_file, 'w') as fout, open(self._filename, 'r') as f1, open(other._filename, 'r') as f2:
-            fout.writelines(f1.readlines() + f2.readlines())
-        return FileReader(new_file)
-    def __add__(self, other):
         base1 = os.path.basename(self._filename)
         base2 = os.path.basename(other._filename)
         dir1 = os.path.dirname(self._filename)
         new_file = os.path.join(dir1, f"{base1}_plus_{base2}.txt")
-        with open(new_file, 'w') as fout, open(self._filename, 'r') as f1, open(other._filename, 'r') as f2:
+        with open(new_file, 'w', encoding='utf-8') as fout, \
+                open(self._filename, 'r', encoding='utf-8') as f1, \
+                open(other._filename, 'r', encoding='utf-8') as f2:
             fout.writelines(f1.readlines() + f2.readlines())
         return FileReader(new_file)
+
     def concat_files(self, *filenames):
-        with open(self._filename, 'a') as fout:
+        with open(self._filename, 'a', encoding='utf-8') as fout:
             for fname in filenames:
-                with open(fname, 'r') as fin:
+                with open(fname, 'r', encoding='utf-8') as fin:
                     fout.write(fin.read())
-    def line_generator(self):
-        with open(self._filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                yield line.rstrip('\n')
-    def deco(color):
-        def decorator(func):
-            def wrapper(*args, **kwargs):
-                print(ANSI_COLORS.get(color, ""), end="")
-                result = func(*args, **kwargs)
-                print(ANSI_COLORS["reset"], end="")
-                return result
-            return wrapper
-        return decorator
-    class UpperCaseFileReader(FileReader):
-        def line_generator(self):
-            with open(self._filename, 'r', encoding='utf-8') as file:
-                for line in file:
-                    yield line.rstrip('\n').upper()
-    class UpperCaseFileReader(FileReader):
+   
+    
+class UpperCaseFileReader(FileReader):
         def line_generator(self):
             with open(self._filename, 'r', encoding='utf-8') as file:
                 for line in file:
@@ -80,35 +84,8 @@ class FileReader:
         def __str__(self):
             with open(self._filename, 'r', encoding='utf-8') as f:
                 return f.read().upper()
-ANSI_COLORS = {
-    "red": "\033[91m",
-    "green": "\033[92m",
-    "yellow": "\033[93m",
-    "cyan": "\033[96m",
-    "magenta": "\033[95m",
-    "reset": "\033[0m"
-}
 
-def deco(color):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            print(ANSI_COLORS.get(color, ""), end="")
-            result = func(*args, **kwargs)
-            print(ANSI_COLORS["reset"], end="")
-            return result
-        return wrapper
-    return decorator
-class UpperCaseFileReader(FileReader):
-    def __init__(self, filename):
-        self._filename = filename
-    def line_generator(self):
-        with open(self._filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                yield line.rstrip('\n').upper()
 
-    def __str__(self):
-        with open(self._filename, 'r', encoding='utf-8') as f:
-            return f.read().upper()
 
 @deco("cyan")
 def show_countdown(seconds):
@@ -121,7 +98,16 @@ def show_countdown(seconds):
             color = "yellow"
         else:
             color = "red"
-        sys.stdout.write(f"{ANSI_COLORS[color]}Time left: {sec} seconds{ANSI_COLORS['reset']}\r")
+        sys.stdout.write(" " * 40 + "\r")  # clear old line
+        sys.stdout.write(f"{ANSI_COLORS[color]}Time left: {sec} second{'s' if sec != 1 else ''}{ANSI_COLORS['reset']}\r")
         sys.stdout.flush()
         time.sleep(1)
     print(f"{ANSI_COLORS['magenta']}Time's up!{ANSI_COLORS['reset']}")
+
+if __name__ == "__main__":
+    try:
+        seconds = int(input("Enter countdown seconds: "))
+        show_countdown(seconds)
+    except ValueError:
+        print(f"{ANSI_COLORS['red']}Invalid input!{ANSI_COLORS['reset']}")
+
